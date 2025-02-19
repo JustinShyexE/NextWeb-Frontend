f0263e80b47f3e155c31da676c10d6c656671971
Iustin Andrei Bacaila <andrei.bacaila15@gmail.com>
2025-02-19 20:44:30 +0000

    setup app router, navlink between pages, basic animations with GSAP on scroll

---
+++ app/homepage/page.tsx
@@ -0,0 +1,3 @@
+export default async function Page() {
+  return <h1>This is the homepage2</h1>;
+}
---
+++ app/style/ui/homepage/homepage.tsx
@@ -0,0 +1,53 @@
+"use client";
+import "../css/homepage.css";
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { useGSAP } from "@gsap/react";
+import { useRef } from "react";
+import Link from "next/link";
+
+export function NavBar() {
+  return (
+    <div className="navbar">
+      <ul>
+        <li>
+          <Link href="/">Home</Link>
+        </li>
+        <li>
+          <Link href="/homepage">This is the secondpage Homepage2...</Link>
+        </li>
+      </ul>
+    </div>
+  );
+}
+
+export function Footer() {
+  return <div className="background">Footer hih</div>;
+}
+
+export function Body() {
+  gsap.registerPlugin(ScrollTrigger);
+  const squareRef = useRef(null);
+  useGSAP(() => {
+    gsap.to(squareRef.current, {
+      duration: 3,
+      rotatioon: 360,
+      scale: 1.2,
+      ease: "linear",
+      scrollTrigger: {
+        trigger: squareRef.current,
+        start: "top 80%", // Starts when the top of square reaches 80% of viewport
+        toggleActions: "play reverse play reverse", // Runs once when triggered
+      },
+    });
+  });
+
+  return (
+    <div className="body">
+      <div className="square" ref={squareRef}></div>
+      <Link href={"http://localhost:3001/homepage"}>
+        <div className="link"></div>
+      </Link>
+    </div>
+  );
+}
